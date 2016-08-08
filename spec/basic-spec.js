f9eb616b44219fa8ecec068095973c0e64f13708
Alhadis <gardnerjohng@gmail.com>
2016-08-08 18:31:53 +1000

    Duplicate look and structure of existing reporter

--- spec/basic-spec.js
+++ spec/basic-spec.js
@@ -10,4 +10,19 @@ describe("This package", function(){
 	it("gets the hose again", function(){
 		this.should.be.extensible.and.ok; // Eventually
 	});
+	
+	it("should fail", function(){
+		NaN.should.equal(NaN); // NaH
+	});
+	
+	describe("Suite nesting", function(){
+		
+		it("does something useful eventually", function(done){
+			setTimeout(_ => done(), 1500);
+		});
+		
+		it("still wakes up unemployed", function(){
+			(!this).should.not.be.true;
+		});
+	});
 });
